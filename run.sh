60f09f80674a815b8d30ad42f06b6ac3e1cb07cf
robertochajin <roberto.chajin@ciberdix.com>
2017-05-22 11:42:43 -0500

    Ajuste a run.sh - agregado sudo

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 sudo mvn clean package -Pdocker
 
 # Se detienen y eliminan los contenedores existentes
-docker-compose stop
-docker-compose rm -f
+sudo docker-compose stop
+sudo docker-compose rm -f
 
 #if [ "$(curl --silent 192.168.99.100:8761/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
 #    #docker-compose up -d auth-service
@@ -14,15 +14,15 @@ docker-compose rm -f
 #fi
 
 # Se inicia el servicio de registro y se espera a que esté listo
-docker-compose up -d discovery-service
+sudo docker-compose up -d discovery-service
 
 while [ -z ${DISCOVERY_SERVICE_READY} ]; do
   echo "Iniciando el servicio de Registro..."
-  if [ "$(curl --silent 192.168.99.100:8761/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+  if [ "$(curl --silent 159.203.69.187:8761/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
       DISCOVERY_SERVICE_READY=true;
   fi
   sleep 2
 done
 
 # Se inician los demás servicios
-docker-compose up -d
+sudo docker-compose up -d
